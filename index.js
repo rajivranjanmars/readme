21a6a9584a28b7bb1e70fda13ca7330e0ab1fe05
Rajiv Ranjan <rajivranjanmars@gmail.com>
2021-09-01 09:40:07 +0530

    idea fullfillment

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-const thename= document.getElementById("thename")
-console.log(name);
+let thename= document.getElementById("thename")
+let username = thename.value
+function readme(){
+
+    //name
+     var nameOfUser = document.getElementById("nameOfUser").value;
+     
+     //github username
+    var GitHubUsername = document.getElementById("GitHubUsername").value;
+
+    //about me
+    var aboutMe = document.getElementById("aboutme").value
+    
+
+    //course and college
+    var study = document.getElementById("study").value;
+   
+   //learn about
+   var learnings = document.getElementById("learn").value;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    let markdown = `
+### Hi ,I Am ${nameOfUser}! <img src=" https://raw.githubusercontent.com/debdutgoswami/debdutgoswami/master/assets/gifs/Hi.gif" width="30px"> \n \n
+
+![](https://komarev.com/ghpvc/?username=${GitHubUsername}&color=blue)
+<h3>${aboutMe}</h3>
+
+- 👨‍🏭 I’m currently pursuing **${study}** 
+- 🏫 I’m currently learning **${learnings}** 
+
+
+
+`;
+
+
+    console.log(markdown);
+    }
