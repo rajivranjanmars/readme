63d587752de690840dd7a0151e396b610e422771
Habeeb David <habeebdh1@gmail.com>
2021-10-07 10:20:37 +0100

    code optimization

---
+++ js/afterload.js
@@ -0,0 +1,37 @@
+// Variables
+var Form1 = document.getElementById('Form1'),
+  Form2 = document.getElementById('Form2'),
+  Form3 = document.getElementById('Form3'),
+  Next1 = document.getElementById('Next1'),
+  Next2 = document.getElementById('Next2'),
+  Back1 = document.getElementById('Back1'),
+  Back2 = document.getElementById('Back2'),
+  progress = document.getElementById('progress');
+
+Next1.onclick = function () {
+  Form1.style.left = '-550px';
+  Form2.style.left = '50px';
+  progress.style.width = '270px';
+};
+Back1.onclick = function () {
+  Form1.style.left = '50px';
+  Form2.style.left = '550px';
+  progress.style.width = '135px';
+};
+Next2.onclick = function () {
+  Form2.style.left = '-550px';
+  Form3.style.left = '50px';
+  progress.style.width = '500px';
+};
+Back2.onclick = function () {
+  Form2.style.left = '50px';
+  Form3.style.left = '550px';
+  progress.style.width = '250px';
+};
+
+// Remove White Space from input
+$(function () {
+  $('input').blur(function () {
+    $(this).val($.trim($(this).val()));
+  });
+});
