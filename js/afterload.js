fde23e077bb73cfadc54490f8b940043c4393e25
Bhupendra Patel <bhupendraptlk@gmail.com>
2022-10-19 01:46:51 +0530

    layouting and fixed issues

--- js/afterload.js
+++ js/afterload.js
@@ -9,22 +9,22 @@ const Form1 = document.getElementById('Form1'),
   progress = document.getElementById('progress');
 
 Next1.onclick = function () {
-  Form1.style.left = '-550px';
-  Form2.style.left = '50px';
+  Form1.style.display= 'none';
+  Form2.style.display = 'block';
   progress.style.width = '270px';
 };
 Back1.onclick = function () {
-  Form1.style.left = '50px';
-  Form2.style.left = '550px';
+  Form1.style.display = 'block';
+  Form2.style.display = 'none';
   progress.style.width = '135px';
 };
 Next2.onclick = function () {
-  Form2.style.left = '-550px';
-  Form3.style.left = '50px';
+  Form2.style.display = 'none';
+  Form3.style.display = 'block';
   progress.style.width = '500px';
 };
 Back2.onclick = function () {
-  Form2.style.left = '50px';
-  Form3.style.left = '550px';
+  Form2.style.display = 'block';
+  Form3.style.display = 'none';
   progress.style.width = '250px';
 };
